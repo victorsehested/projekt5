92f751b32d3b6c918b4ee60a600b5b117c6d0459
Victor Sehested <victorsehested@gmail.com>
2021-04-16 19:42:49 +0200

    Kontakt siden tilføjet

---
+++ Projekt5/kontakt.js
@@ -0,0 +1,29 @@
+const navn = document.getElementById('navn')
+const email = document.getElementById('email')
+const besked = document.getElementById('besked')
+
+const errorElement = document.getElementById('error')
+const form = document.getElementById('form')
+
+
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (navn.value === '' || navn.value == null) {
+        messages.push('Navn er påkrævet')
+    }
+
+    if (email.value.length <= 3) {
+        messages.push('Email er påkrævet')
+    }
+
+    if (besked.value.length <= 50) {
+        messages.push('Din besked skal være længere end 50 tegn')
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
+
+})
